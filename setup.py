bfc9feb357c806c84d6d8e6e25c06b1aad79d6f9
explosivo22 <barbourbj@gmail.com>
2021-07-01 17:30:18 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="rinnaicontrolr",
+    name="aio-rinnaicontrolr",
     version="0.2.1.a1",
     description="Python interface for Rinnai Control-R API",
-    url="https://github.com/explosivo22/rinnaicontrolr",
+    url="https://github.com/explosivo22/aio-rinnaicontrolr",
     author="Brad Barbour",
     author_email="barbourbj@gmail.com",
     license='Apache Software License',
     install_requires=[ 'aiohttp>=3.7.0' ],
     keywords=[ 'rinnai', 'home automation', 'water heater' ],
-    packages=[ 'rinnaicontrolr' ],
+    packages=[ 'aio-rinnaicontrolr' ],
     zip_safe=True,
     classifiers=[
         "Programming Language :: Python :: 3",
