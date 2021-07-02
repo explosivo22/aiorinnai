20cc891916b72c37201b48e32054e689ceb2c0e3
explosivo22 <barbourbj@gmail.com>
2021-07-01 19:01:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
-import setuptools
+import os.path
+from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+# The text of the README file
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
 
 setuptools.setup(
-    name="aio-rinnaicontrolr",
+    name="api-rinnaicontrolr",
     version="0.2.1.a1",
     description="Python interface for Rinnai Control-R API",
-    url="https://github.com/explosivo22/aio-rinnaicontrolr",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/explosivo22/rinnaicontrolr",
     author="Brad Barbour",
     author_email="barbourbj@gmail.com",
     license='Apache Software License',
     install_requires=[ 'aiohttp>=3.7.0' ],
     keywords=[ 'rinnai', 'home automation', 'water heater' ],
-    packages=[ 'aio-rinnaicontrolr' ],
+    packages=[ 'rinnaicontrolr' ],
     zip_safe=True,
     classifiers=[
         "Programming Language :: Python :: 3",
