7aa23d146216c09309af9d351154c7176ac729cf
Brad Barbour <barbourbj@gmail.com>
2021-07-01 19:47:31 -0500

    change version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except FileNotFoundError:
 
 setup(
     name="aio-rinnaicontrolr",
-    version="0.2.1.a3",
+    version="0.2.1.a4",
     description="Python interface for Rinnai Control-R API",
     long_description=long_description,
     long_description_content_type='text/markdown',
