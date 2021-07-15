c00e1341e8eb5a4acf769f8ec3a4c55c7c3a45d2
Brad Barbour <barbourbj@gmail.com>
2021-07-14 20:35:20 -0500

    update version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except FileNotFoundError:
 
 setup(
     name="aiorinnai",
-    version="0.2.1.a10",
+    version="0.2.1.a11",
     description="Python interface for Rinnai Control-R API",
     long_description=long_description,
     long_description_content_type='text/markdown',
