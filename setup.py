8bc352568393f9d5353bea4eee951d03cb05ec58
Brad Barbour <barbourbj@gmail.com>
2022-05-06 21:35:04 -0500

    update aioboto3 to 9.6.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except FileNotFoundError:
 
 setup(
     name="aiorinnai",
-    version="0.2.7",
+    version="0.2.8",
     description="Python interface for Rinnai Control-R API",
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -21,7 +21,7 @@ setup(
     author="Brad Barbour",
     author_email="barbourbj@gmail.com",
     license='Apache Software License',
-    install_requires=[ 'aiohttp>=3.7.0', 'aioboto3' ],
+    install_requires=[ 'aiohttp>=3.7.0', 'aioboto3>=9.6.0' ],
     keywords=[ 'rinnai', 'home automation', 'water heater' ],
     packages=[ 'aiorinnai' ],
     zip_safe=True,
