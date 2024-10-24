7adaf37596a31fb02fb063a5e96ff38275d0de85
Explosivo22 <barbourbj@gmail.com>
2024-10-24 14:31:35 -0500

    bump aioboto3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except FileNotFoundError:
 
 setup(
     name="aiorinnai",
-    version="0.3.3",
+    version="0.3.4",
     description="Python interface for Rinnai Control-R API",
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -21,7 +21,7 @@ setup(
     author="Brad Barbour",
     author_email="barbourbj@gmail.com",
     license='Apache Software License',
-    install_requires=[ 'aiohttp>=3.7.0', 'aioboto3==13.1.1' ],
+    install_requires=[ 'aiohttp>=3.7.0', 'aioboto3==13.2.0' ],
     keywords=[ 'rinnai', 'home automation', 'water heater' ],
     packages=[ 'aiorinnai' ],
     zip_safe=True,
